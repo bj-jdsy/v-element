0f3b3fbb920cf4bf7fafd6d3bcdc46164874f1f4
pingguanfei <pingguanfei@bjjdsy.com.cn>
2023-02-06 15:04:44 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<JTable column="abc" :data="dd" :border="false" :show-header="true" :stripe="false" @row-click="bbb"
+		:data-options="{data:dd, stripe:true}">
+	</JTable>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+	import JTable from './components/table/CTable.vue'
+	const bbb = () => {
+		console.log('ccc')
+	}
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	const dd = [{
+			date: '2016-05-03',
+			name: 'Tom',
+			address: 'No. 189, Grove St, Los Angeles',
+		},
+		{
+			date: '2016-05-02',
+			name: 'Tom',
+			address: 'No. 189, Grove St, Los Angeles',
+		},
+		{
+			date: '2016-05-04',
+			name: 'Tom',
+			address: 'No. 189, Grove St, Los Angeles',
+		},
+		{
+			date: '2016-05-01',
+			name: 'Tom',
+			address: 'No. 189, Grove St, Los Angeles',
+		},
+	]
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/table/CTable.vue
@@ -0,0 +1,43 @@
+<template>
+	<div>
+		<el-table ref="" v-bind="dataOptions" style="width: 100%">
+			<el-table-column prop="date" label="Date" width="180" />
+			<el-table-column prop="name" label="Name" width="180" />
+			<el-table-column prop="address" label="Address" />
+		</el-table>
+		<el-pagination layout="prev, pager, next" :total="1000" />
+	</div>
+</template>
+<script>
+	import {
+		computed
+	} from 'vue'
+
+	export default {
+		name: 'JTable2',
+		props: {
+		},
+		setup(props, {
+			attrs
+		}) {
+			const dataOptions = computed(() => {
+				console.log("attrs", attrs)
+				console.log("prop", props)
+				const bindValue = {
+					...attrs.dataOptions,
+					...attrs,
+					...props,
+				}
+				console.log(bindValue)
+				return bindValue
+
+			})
+			return {
+				dataOptions
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
